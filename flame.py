f6bc5944d8c0c9e68b00736bf21777acb36e244c
Sam Mosleh <sam.mosleh@ut.ac.ir>
2019-07-08 07:46:05 +0430

    Now the agent is somehow smarter and some model added to see the result

--- flame.py
+++ flame.py
@@ -13,14 +13,15 @@
 
 import random
 import time
-
+import math
 import game
 import util
 from captureAgents import CaptureAgent
 from game import Directions
 
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers.core import Dense, Dropout
+import numpy as np
 
 #################
 # Team creation #
@@ -30,8 +31,8 @@ from keras.layers.core import Dense, Dropout
 def createTeam(firstIndex,
                secondIndex,
                isRed,
-               first='ReinforcementAgent',
-               second='ReinforcementAgent',
+               first='DeepReinforcementAgent',
+               second='DeepReinforcementAgent',
                numTraining=0):
     """
     This function should return a list of two agents that will form the
@@ -73,6 +74,7 @@ class ReinforcementAgentBase(CaptureAgent):
         self.numTraining = int(numTraining)
         self.has_no_observation = True
         self.episodesSoFar = 0
+        self.actionHistory = []
 
     def isInTraining(self):
         return self.episodesSoFar < self.numTraining
@@ -83,14 +85,17 @@ class ReinforcementAgentBase(CaptureAgent):
     def getLegalActions(self, state):
         return state.getLegalActions(self.index)
 
+    def makeUpdate(self):
+        delta_reward = self.getScore(self.getCurrentObservation()) - \
+                       self.getScore(self.getPreviousObservation())
+        self.update(self.getPreviousObservation(), self.actionHistory[-2],
+                    self.getCurrentObservation(), delta_reward)
+        # episode rewards?
+
     def observationFunction(self, state):
         if not self.has_no_observation and \
            self.getPreviousObservation() is not None:
-            delta_reward = self.getCurrentObservation().getScore() - \
-                           self.getPreviousObservation().getScore()
-            self.update(self.getPreviousObservation(), self.last_action,
-                        self.getCurrentObservation(), delta_reward)
-            # episode rewards?
+            self.makeUpdate()
         return CaptureAgent.observationFunction(self, state)
 
     def update(self, state, action, nextState, reward):
@@ -98,8 +103,8 @@ class ReinforcementAgentBase(CaptureAgent):
 
     def chooseAction(self, gameState):
         self.has_no_observation = False
-        self.last_action = self.actionSelector(gameState)
-        return self.last_action
+        self.actionHistory.append(self.actionSelector(gameState))
+        return self.actionHistory[-1]
 
     def actionSelector(self, gameState):
         if util.flipCoin(self.epsilon):
@@ -111,17 +116,13 @@ class ReinforcementAgentBase(CaptureAgent):
         # starting episode?
 
     def final(self, gameState):
-        delta_reward = self.getCurrentObservation().getScore() - \
-                           self.getPreviousObservation().getScore()
-        self.update(self.getPreviousObservation(), self.last_action,
-                    self.getCurrentObservation(), delta_reward)
-        # episode rewards?
+        self.makeUpdate()
         self.stopEpisode()
         if self.isInTraining():
             print 'Training:{'
         else:
             print 'Testing:{'
-        print self.getCurrentObservation().getScore()
+        print self.getScore(self.getCurrentObservation())
         print '}'
         CaptureAgent.final(self, gameState)
 
@@ -197,25 +198,44 @@ class DeepReinforcementAgent(ReinforcementAgentBase):
                  index,
                  timeForComputing=0.1,
                  learning_rate=0.3,
-                 exploration_rate=0.25,
+                 exploration_rate=0.3,
                  discount=0.8,
-                 numTraining=0):
+                 numTraining=0,
+                 createNewModel=False):
         ReinforcementAgentBase.__init__(self, index, timeForComputing,
                                         learning_rate, exploration_rate,
                                         discount, numTraining)
-        self.q_estimator = None
+        self.createNewModel = createNewModel
+
+    def registerInitialState(self, gameState):
+        global nn
+        self.startPos = gameState.getAgentPosition(self.index)
+        if nn is None:
+            if self.createNewModel:
+                nn = NeuralNetwork(gameState)
+            else:
+                nn = NeuralNetwork(gameState, modelPath='myModel.h5')
+        self.q_estimator = nn
+        ReinforcementAgentBase.registerInitialState(self, gameState)
 
     def getQValue(self, state, action):
         if self.q_estimator is None:
             self.q_estimator = NeuralNetwork(state)
-        prediction = self.q_estimator.predict(state, action)
+        prediction = self.q_estimator.predict(self, state, action)
         return prediction
 
     def update(self, state, action, nextState, reward):
         if self.q_estimator is None:
             self.q_estimator = NeuralNetwork(state)
         estimated_q = reward + self.gamma * self.getValue(nextState)
-        self.q_estimator.update(state, action, estimated_q)
+        estimated_q -= 4 / math.sqrt(
+            self.getMazeDistance(state.getAgentPosition(self.index),
+                                 self.startPos) + 1)
+        self.q_estimator.update(self, state, action, estimated_q)
+
+    def final(self, gameState):
+        self.q_estimator.save('myModel.h5')
+        ReinforcementAgentBase.final(self, gameState)
 
 
 class NeuralNetwork:
@@ -223,26 +243,75 @@ class NeuralNetwork:
 
     """
 
-    def __init__(self, state, firstLayerSize=200):
+    def __init__(self, state, firstLayerSize=300, modelPath=None):
         walls = state.getWalls()
         self.width = walls.width
         self.height = walls.height
         self.area = self.width * self.height
+        if modelPath is None:
+            self.model = Sequential()
+            self.model.add(
+                Dense(units=firstLayerSize,
+                      activation='relu',
+                      input_dim=6 * self.area,
+                      init='random_uniform'))
+            self.model.add(Dense(units=1, activation='linear'))
+            self.model.compile(optimizer='adam', loss='mean_squared_error')
+            self.model.summary()
+        else:
+            self.model = load_model(modelPath)
 
-        self.model = Sequential()
-        self.model.add(
-            Dense(units=200,
-                  activation='relu',
-                  input_dim=6 * self.size,
-                  init='lecun_uniform'))
-        self.model.add(Dense(units=1, activation='linear'))
+    def predict(self, agent, state, action):
+        encodedState = self.encodeState(agent, state, action)
+        return self.model.predict(encodedState)[0][0]
 
-    def predict(self, state, action):
-        input = self.makeInputFrom(state, action)
-        return 1
+    def update(self, agent, state, action, q):
+        encodedState = self.encodeState(agent, state, action)
+        self.model.fit(encodedState, [q], batch_size=1, epochs=1, verbose=0)
 
-    def makeInputFrom(self, state, action):
-        pass
+    def encodeState(self, agent, state, action):
+        # Get all the foods on the ground
+        allFoods = np.zeros((self.width, self.height))
+        for x, y in agent.getFood(state).asList():
+            allFoods[x, y] = 1
+        for x, y in agent.getFoodYouAreDefending(state).asList():
+            allFoods[x, y] = 1
+        # get all the capsules
+        allCapsuls = np.zeros((self.width, self.height))
+        for x, y in agent.getCapsules(state):
+            allCapsuls[x, y] = 1
+        for x, y in agent.getCapsulesYouAreDefending(state):
+            allCapsuls[x, y] = 1
+        # get all the enemies if you can :D
+        allEnemies = np.zeros((self.width, self.height))
+        for enemyIndex in agent.getOpponents(state):
+            enemyPos = state.getAgentPosition(enemyIndex)
+            if enemyPos is not None:
+                allEnemies[enemyPos] = 1
+        # get teammate position and set
+        teamMate = np.zeros((self.width, self.height))
+        agent_1, agent_2 = agent.getTeam(state)
+        if agent.index == agent_1:
+            teamMatePosition = state.getAgentPosition(agent_2)
+        else:
+            teamMatePosition = state.getAgentPosition(agent_1)
+        teamMate[teamMatePosition] = 1
+        # set the Agent position
+        agentPosition = np.zeros((self.width, self.height))
+        myPos = state.getAgentPosition(agent.index)
+        agentPosition[myPos[0], myPos[1]] = 1
+        # set the next position
+        agentNextPosition = np.zeros((self.width, self.height))
+        successor = state.generateSuccessor(agent.index, action)
+        nextPos = successor.getAgentPosition(agent.index)
+        agentNextPosition[int(nextPos[0]), int(nextPos[1])] = 1
+        result = np.concatenate((allFoods, allCapsuls, allEnemies, teamMate,
+                                 agentPosition, agentNextPosition)).reshape(
+                                     (1, -1))
+        return result
+
+    def save(self, pathname):
+        self.model.save(pathname)
 
 
 class DummyAgent(CaptureAgent):
@@ -271,7 +340,6 @@ class DummyAgent(CaptureAgent):
         CaptureAgent.registerInitialState in captureAgents.py.
         '''
         CaptureAgent.registerInitialState(self, gameState)
-        self.plays = 0
 
     def chooseAction(self, gameState):
         """
@@ -281,5 +349,33 @@ class DummyAgent(CaptureAgent):
         '''
         You should change this in your own agent.
         '''
-        self.plays += 1
+        #walls = gameState.getWalls()
+        #width = walls.width
+        #height = walls.height
+        #print width, height
+        #print self.index
+        #print self.getFood(gameState).asList()
+        #print '>>>>>>>>>>', gameState.getAgentPosition(0)
+        #print self.getTeam(gameState), self.getOpponents(gameState)
+        #print gameState.getAgentState(self.index).getPosition()
+        #print gameState.getAgentState(0).getPosition()
+        #print gameState.getAgentState(0).getDirection()
+        #print gameState.getAgentState(0).isPacman
+        #x = self.getCurrentObservation()
+        #x = self.getCurrentObservation()
+        #x = self.getCurrentObservation()
+        #for i in range(width):
+        #    for j in range(height):
+        #        if x[i][j] != gameState[i][j]:
+        #            print i, j, x[i][j], gameState[i][j]
+        #print self.getFood(gameState)
+        #print '**************************************************'
+        #print self.getFoodYouAreDefending(gameState)
+        #print '**************************************************'
+        #print gameState
+        #print self.getCurrentObservation()
+        #print np.array(gameState, dtype=np.int)
         return random.choice(actions)
+
+
+nn = None
